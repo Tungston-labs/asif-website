d19df5e230d1a361d35b0a1781c8079e195d159a
pranav370 <pranav@tungstonlabs.com>
2026-05-05 12:34:50 +0530

    Alignment fixed in projects section of portfolio page

--- components/Portfolio/Project/projects.style.ts
+++ components/Portfolio/Project/projects.style.ts
@@ -4,8 +4,8 @@ export const Container = styled.section`
    width: 100%;
   padding: 2rem 2rem 6.25rem;
   background: #ffffff;
-    @media (max-width: 600px) {
-  margin-left:-1.5rem;
+    @media (max-width: 768px) {
+  margin-left:-3.3rem;
 
   }
 `;
@@ -27,7 +27,7 @@ export const Wrapper = styled.div`
   @media (max-width: 600px) {
     flex-direction: column;
     gap: 2.5rem;
-    max-width:22rem
+    max-width:26rem
   }
   @media (max-width: 768px) {
     flex-direction: column;
