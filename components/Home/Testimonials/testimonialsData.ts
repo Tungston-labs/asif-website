72ab37041299fd79e91c8453b6fd24b512a80f8b
Tungston-Labs <support@tungstonlabs.com>
2026-07-13 16:41:20 +0530

    testimonials

--- components/Home/Testimonials/testimonialsData.ts
+++ components/Home/Testimonials/testimonialsData.ts
@@ -1,5 +1,4 @@
 // testimonialsData.ts
-
 export interface Testimonial {
   id: number;
   image: string;
@@ -10,20 +9,28 @@ export interface Testimonial {
 
 export const testimonialsData: Testimonial[] = [
   {
+    id: 7,
+    image: "/images/K.CherianThomas.jpeg",
+    name: "K. Cherian Thomas ",
+    place: "Ernakulam, Kadavantra ",
+    content:
+      "Working with Architect Asif Ahmed was a wonderful experience from start to finish. He took the time to understand our vision and transformed it into a home that perfectly reflects our lifestyle. Throughout the entire project, he was approachable, patient, and always willing to listen to our ideas. No matter how many revisions or changes we requested, he never hesitated to accommodate them and ensured every detail met our expectations. His professionalism, creativity, and commitment to quality made the entire journey smooth and stress-free. We are truly grateful for his dedication and couldn't be happier with our beautiful home. I would highly recommend Asif Ahmed to anyone looking for an architect who genuinely cares about bringing their clients' vision to life.",
+  },
+  {
+    id: 6,
+    image: "/images/Jasykumaran.jpeg",
+    name: "Jasy kumaran ",
+    place: "Kanichukulangara Alappuzha",
+    content:
+      "We are absolutely delighted with our beautiful home, La Grande Vue, Kanichukulangara.Our architect Mr. Asif and his team transformed our vision into reality with a stunning design and exceptional attention even to the small details. The project was professionally supervised from start to finish , ensuring outstanding quality throughout. We truly appreciate the creativity, dedication, and commitment, and highly recommend their services to anyone looking for an architect who delivers excellence.",
+  },
+  {
     id: 1,
     image: "/images/AjayanVarghese.jpeg",
     name: "Ajayan Varghese ",
     place: "Kottayam",
     content:
       "Your ability to balance aesthetic elegance with functional, everyday design is truly impressive. From our initial concepts to the final execution, your guidance and expertise ensured that the project surpassed all my expectations. The structure stands as a testament to your talent and vision.",
-  },
-  {
-    id: 2,
-    image: "/images/PramothSakthi.jpeg",
-    name: "Pramoth Sakthi",
-    place: "Bangalore ",
-    content:
-      "Huge thanks to Asif and his team for bringing our dream home to life and exceeding all our expectations.You managed the project beautifully, and the team's talent truly shines through in every corner.",
   },
   {
     id: 3,
@@ -34,12 +41,20 @@ export const testimonialsData: Testimonial[] = [
       "Our dream home became a reality thanks to Ar. Asif Ahmad. His creativity, attention to detail, and commitment to excellence are reflected in every corner of our home. The design is modern, functional, and perfectly suited to our lifestyle. We are truly grateful for his outstanding work and highly recommend him to anyone planning to build their dream home.",
   },
   {
-    id: 4,
-    image: "/images/JohnT.M.jpeg",
-    name: "John T.M",
-    place: "Koothattukulam",
+    id: 10,
+    image: "/images/Mr&MrsAbdulKader.jpeg",
+    name: "Mr & Mrs Abdul Kader. ",
+    place: "Kannur",
     content:
-      "We had a great experience working with Architect Asif Ahmad. He understood our requirements perfectly and designed a beautiful home that exceeded our expectations.",
+      "Working with Mr. Asif has been a wonderful experience. He was always approachable, patient, and genuinely invested in bringing our vision to life. Every design discussion was thoughtful, and his guidance in choosing the right materials and practical solutions made the entire process smooth and stress-free. His professionalism, creativity, and attention to detail gave us complete confidence throughout the project. We're truly grateful for his dedication and highly recommend him to anyone looking for an architect they can trust.",
+  },
+  {
+    id: 11,
+    image: "/images/Anwarsadath.jpeg",
+    name: "Anwar sadath ",
+    place: "Elamakkara, Ernakulam",
+    content:
+      "We are absolutely delighted with Cocoon, designed by Architect Asif. The contemporary three-floor home, with its unique Z-shaped architecture, lift, spacious interiors, elegant colour palette, and striking exterior lighting, is unlike any other house. Every space is beautifully planned and thoughtfully executed. We truly love every detailing and highly recommend Architect Asif for creating exceptional homes.",
   },
   {
     id: 5,
@@ -50,11 +65,35 @@ export const testimonialsData: Testimonial[] = [
       "From the first sketch to the final detail, you understood our dream perfectly. In a world full of similar designs, your work stands out with its own unique signature. Thank you for giving us a home that is beautiful, meaningful, and filled with heart. Your work will be part of our family’s memories forever.",
   },
   {
-    id: 6,
-    image: "/images/Jasykumaran.jpeg",
-    name: "Jasy kumaran ",
-    place: "Kanichukulangara Alappuzha",
+    id: 9,
+    image: "/images/AjiHassan&RasheedaAji.jpeg",
+    name: "Aji Hassan & Rasheeda Aji ",
+    place: "Chengannoor",
     content:
-      "We are absolutely delighted with our beautiful home, La Grande Vue, Kanichukulangara.Our architect Mr. Asif and his team transformed our vision into reality with a stunning design and exceptional attention even to the small details. The project was professionally supervised from start to finish , ensuring outstanding quality throughout. We truly appreciate the creativity, dedication, and commitment, and highly recommend their services to anyone looking for an architect who delivers excellence.",
+      "From our first discussion, Ar. Asif understood exactly what we wanted for our contemporary home. He transformed every requirement into a practical and elegant design, creating a home that exceeded our expectations. His creativity, professionalism, and attention to detail made the entire journey smooth and enjoyable. We are delighted with the outcome and highly recommend him to anyone looking for an architect who can turn ideas into reality.",
+  },
+  {
+    id: 8,
+    image: "/images/DrBijoyBalakrishnn.jpeg",
+    name: "Dr.Bijoy Balakrishnan",
+    place: "Varappuzha ",
+    content:
+      "Asif Ahmed transformed our vision into a home that is truly unique—bold in design, yet warm and deeply liveable. His creativity, attention to detail, and ability to blend architecture with our lifestyle made the entire journey special. We are incredibly proud of the home he created for us. With him we got a home , not just a house.",
+  },
+  {
+    id: 2,
+    image: "/images/PramothSakthi.jpeg",
+    name: "Pramoth Sakthi",
+    place: "Bangalore ",
+    content:
+      "Huge thanks to Asif and his team for bringing our dream home to life and exceeding all our expectations.You managed the project beautifully, and the team's talent truly shines through in every corner.",
+  },
+  {
+    id: 4,
+    image: "/images/JohnT.M.jpeg",
+    name: "John T.M",
+    place: ".00",
+    content:
+      "We had a great experience working with Architect Asif Ahmad. He understood our requirements perfectly and designed a beautiful home that exceeded our expectations.",
   },
 ];
